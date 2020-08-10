4e5135ace41b88de568deb5c0a32364aeeb8d685
janaguiotti <jana.guiotti@gmail.com>
2020-08-10 10:38:58 -0300

    finishing brand bar

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
 import Brastemp from '../assets/images/brastemp.png';
 import CompraCerta from '../assets/images/compra-certa.png';
 import Consul from '../assets/images/consul.png';
@@ -9,20 +8,30 @@ import '../styles/Brands.scss';
 class Brands extends React.Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <Row>
-                            <Col className="title"><h3>Nossas principais lojas VTEX</h3></Col>
-                            <Col><i className="fa fa-long-arrow-right" aria-hidden="true"></i></Col>
-                        </Row>
-                    </Col>
-                    <Col><img src={Brastemp} alt="Brastemp" /></Col>
-                    <Col><img src={CompraCerta} alt="Compra Certa" /></Col>
-                    <Col><img src={Consul} alt="Consul" /></Col>
-                    <Col><img src={TheBar} alt="The Bar" /></Col>
-                </Row>
-            </Container>
+            <div className="grid-container">
+                <div className="grid-item">
+                    <div className="grid-container">
+                        <div className="grid-item">
+                            <div className="title"><h3>Nossas principais lojas VTEX</h3></div>
+                        </div>
+                        <div className="grid-item">
+                            <div className="icon"><i className="fa fa-long-arrow-right" aria-hidden="true"></i></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid-item">
+                    <div className="image"><img src={Brastemp} alt="Brastemp" /></div>
+                </div>
+                <div className="grid-item">
+                    <div className="image"><img src={CompraCerta} alt="Compra Certa" /></div>
+                </div>
+                <div className="grid-item">
+                    <div className="image"><img src={Consul} alt="Consul" /></div>
+                </div>
+                <div className="grid-item">
+                    <div className="image"><img src={TheBar} alt="The Bar" /></div>
+                </div>
+            </div>
         );
     }
 }
