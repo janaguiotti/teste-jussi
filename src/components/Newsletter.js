7762a210a562844404714d711f408a88673d44f9
janaguiotti <jana.guiotti@gmail.com>
2020-08-10 11:40:32 -0300

    finishing newsletter

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { Container, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
 import AspasVerde from '../assets/images/aspas-verde.png';
 import '../styles/Newsletter.scss';
 
 class Newsletter extends React.Component {
     render() {
         return (
-            <Container>
+            <Container className="subscribe">
                 <Row>
                     <Col><h2><img src={AspasVerde} alt="Aspas" /> receba novidades da nossa área de produtos digitais.</h2></Col>
                 </Row>
                 <Row>
                     <Col>
-                        <Form.Group>
-                            <Form.Control className="news-input" size="lg" type="text" placeholder="Digite seu e-mail" />
-                        </Form.Group>
-                        <Button className="news-button" variant="primary" type="submit">
-                            CADASTRAR
-                        </Button>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                placeholder="Digite seu e-mail"
+                                aria-label="Digite seu e-mail"
+                                aria-describedby="basic-addon2"
+                            />
+                            <InputGroup.Append>
+                            <Button variant="outline-secondary">CADASTRAR</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
                     </Col>
                 </Row>
             </Container>
